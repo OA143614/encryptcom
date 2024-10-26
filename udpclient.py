b14170f3714575491f83e5879ba4d93ae01bb9cb
unknown <ann.ananong@gmail.com>
2024-10-26 15:51:16 -0400

    done with AES

--- udpclient.py
+++ udpclient.py
@@ -1,86 +1,52 @@
 import socket
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
-from cryptography.hazmat.backends import default_backend
-import os
 import threading
-import msvcrt
-import sys
+from Cryptodome.Cipher import AES
+import os
 
-# Client settings
+# Server settings
 HOST = '127.0.0.1'
 PORT = 65432
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.connect((HOST, PORT))
 
-# Request public key from server
-client.sendto(b"REQUEST_PUBLIC_KEY", (HOST, PORT))
-try:
-    public_pem, _ = client.recvfrom(8192)  # Increased buffer size
-except ConnectionResetError as e:
-    print(f"Connection error: {e}")
-    exit()
+# Display connection info
+print(f"Connected to server: {HOST}:{PORT}")
 
-# Load public key
-public_key = serialization.load_pem_public_key(public_pem)
-
-# Generate RC4 key
-rc4_key = os.urandom(16)
-
-# Encrypt RC4 key with server's public key
-encrypted_key = public_key.encrypt(
-    rc4_key,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
-
-# Send encrypted RC4 key to server
-client.sendto(b"KEY:" + encrypted_key, (HOST, PORT))
-response, _ = client.recvfrom(1024)
-print(response.decode())
-
-# Define the cipher globally
-cipher = Cipher(algorithms.ARC4(rc4_key), mode=None, backend=default_backend())
+# Use the same fixed AES key as the server
+aes_key = b'This is a key123This is a key123'  # 32 bytes
 
 # Function to receive messages from the server
 def receive_messages(client_socket):
     while True:
         try:
-            encrypted_response, _ = client_socket.recvfrom(1024)
-            decryptor = cipher.decryptor()
-            response = decryptor.update(encrypted_response)
-            print(f"Received response from server: {response.decode()}")
+            message, addr = client_socket.recvfrom(1024)
+            if message:
+                # Decrypt the message using the AES key
+                iv = message[:16]
+                encrypted_message = message[16:]
+                cipher = AES.new(aes_key, AES.MODE_CFB, iv)
+                decrypted_message = cipher.decrypt(encrypted_message)
+                print(f"Server: {decrypted_message.decode('utf-8', errors='ignore')}")
         except Exception as e:
             print(f"Error receiving message: {e}")
             break
 
-# Start the thread to handle incoming messages
-thread = threading.Thread(target=receive_messages, args=(client,))
+# Start a thread to receive messages
+thread = threading.Thread(target=receive_messages, args=(client_socket,))
+thread.daemon = True
 thread.start()
 
 while True:
-    sentence = ''
-    while True:
-        if msvcrt.kbhit():
-            char = msvcrt.getwch()
-            if char == '\r':  # Check for Enter key (carriage return)
-                break
-            sentence += char
-            sys.stdout.write(char)
-            sys.stdout.flush()
-    if sentence:
-        message = sentence.encode()
-        encryptor = cipher.encryptor()
-        encrypted_message = encryptor.update(message)
-
-        # Send encrypted message to server
-        client.sendto(encrypted_message, (HOST, PORT))
-
-        sys.stdout.write(f"\n<You> {sentence}\n")
-        sys.stdout.flush()
-
-client.close()
+    try:
+        sentence = input("Enter message to send: ")
+        if sentence:
+            iv = os.urandom(16)
+            cipher = AES.new(aes_key, AES.MODE_CFB, iv)
+            encrypted_message = iv + cipher.encrypt(sentence.encode('utf-8'))
+            client_socket.sendto(encrypted_message, (HOST, PORT))
+            print(f"You: {sentence}")
+    except KeyboardInterrupt:
+        print("\nConnection closed.")
+        client_socket.close()
+        break
--- udpserver.py
+++ udpserver.py
@@ -1,23 +1,14 @@
 import socket
 import threading
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
-from cryptography.hazmat.backends import default_backend
-
-# Generate RSA keys
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-public_key = private_key.public_key()
-
-# Serialize public key to send to client
-public_pem = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
+from Cryptodome.Cipher import AES
+import os
 
 # Server settings
 HOST = '127.0.0.1'
 PORT = 65432
+
+# Use a fixed AES key for simplicity
+aes_key = b'This is a key123This is a key123'  # 32 bytes
 
 # List to keep track of connected clients
 clients = []
@@ -27,52 +18,31 @@ server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((HOST, PORT))
 print(f"Server listening on {HOST}:{PORT}")
 
-rc4_key = None
-
-def broadcast(message, sender_addr, sender_info):
+def broadcast(message, sender_addr):
     for client in clients:
         if client != sender_addr:
             try:
-                full_message = f"{sender_info}: {message.decode()}"
-                server.sendto(full_message.encode(), client)
+                server.sendto(message, client)
             except:
                 clients.remove(client)
 
 def handle_messages():
-    global rc4_key
     while True:
         message, addr = server.recvfrom(4096)
-        if message == b"REQUEST_PUBLIC_KEY":
-            server.sendto(public_pem, addr)
-        elif message.startswith(b"KEY:"):
-            encrypted_key = message[4:]
-            rc4_key = private_key.decrypt(
-                encrypted_key,
-                padding.OAEP(
-                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                    algorithm=hashes.SHA256(),
-                    label=None
-                )
-            )
-            print(f"Received RC4 key: {rc4_key}")
-            response = b"RC4 key received!"
-            server.sendto(response, addr)
-        else:
-            if rc4_key:
-                if addr not in clients:
-                    clients.append(addr)
-                # Decrypt message from client
-                cipher = Cipher(algorithms.ARC4(rc4_key), mode=None, backend=default_backend())
-                decryptor = cipher.decryptor()
-                decrypted_message = decryptor.update(message)
-                print(f"Received message from {addr}: {decrypted_message.decode()}")
-                
-                # Encrypt message and send to clients
-                sender_info = f"{addr[0]}:{addr[1]}"
-                response = b"Hello from server!"
-                encryptor = cipher.encryptor()
-                encrypted_response = encryptor.update(response)
-                broadcast(encrypted_response, addr, sender_info)
+        if addr not in clients:
+            clients.append(addr)
+        # Decrypt message from client
+        iv = message[:16]
+        encrypted_message = message[16:]
+        cipher = AES.new(aes_key, AES.MODE_CFB, iv)
+        decrypted_message = cipher.decrypt(encrypted_message)
+        print(f"Received message from {addr}: {decrypted_message.decode('utf-8', errors='ignore')}")
+        
+        # Encrypt message and send to clients
+        iv = os.urandom(16)
+        cipher = AES.new(aes_key, AES.MODE_CFB, iv)
+        encrypted_response = iv + cipher.encrypt(decrypted_message)
+        broadcast(encrypted_response, addr)
 
 # Start the thread to handle incoming messages
 thread = threading.Thread(target=handle_messages)
